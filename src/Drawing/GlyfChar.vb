50b543363424966522a5195c5a4a80fecd04fba0
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-11-05 15:34:45 +0300

    Create GlyfChar.vb

---
+++ src/Drawing/GlyfChar.vb
@@ -0,0 +1,15 @@
+Namespace Drawing
+
+    ''' <summary>
+    ''' Представляет глиф символа.
+    ''' </summary>
+    Public Structure GlyfChar
+
+        Public CodeChar As UShort
+        Public Unicode As UShort
+        Public Width As Double
+
+    End Structure
+
+End Namespace
+
